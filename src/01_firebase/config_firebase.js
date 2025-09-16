761263aa36b1c59ef5ff8173358cf158b2040aa9
Nick127k <Nick127k@iastate.edu>
2025-09-16 12:11:37 -0400

    This should officially work

--- src/01_firebase/config_firebase.js
+++ src/01_firebase/config_firebase.js
@@ -3,15 +3,15 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBQkI6S5L7BkxtZscmdgkRXrSC79uDegHU",
   authDomain: "cpre329project1.firebaseapp.com",
   projectId: "cpre329project1",
-  storageBucket: "challo-ghume-fa22c.appspot.com",
-  messagingSenderId: "411994731743",
-  appId: "1:411994731743:web:a02cec85eb6aa66ddbe56a",
-  measurementId: "G-5KYMV9KZ2D"
+  storageBucket: "cpre329project1.firebasestorage.app",
+  messagingSenderId: "301882557606",
+  appId: "1:301882557606:web:6cc045a33ada644bd8e067",
+  measurementId: "G-W6FN0YWFYV"
 };
 
 // Initialize Firebase
